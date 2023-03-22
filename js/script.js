3703a9ccb5930dbb1e0484342685b976f8b654a1
tinuola <24995224+tinuola@users.noreply.github.com>
2023-03-22 01:36:45 -0400

    Adds overlay effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello!')
+const openMenuBtn = document.querySelector('#open-menu');
+
+const closeMenuBtn = document.querySelector('#close-menu');
+
+const menu = document.querySelector('nav ul');
+
+const openIcon = './assets/images/icon-menu.svg';
+
+const closeIcon = './assets/images/icon-menu-close.svg';
