6359b9b37086eb59c431aa98113b6415a7af59e0
tinuola <24995224+tinuola@users.noreply.github.com>
2023-03-23 23:36:49 -0700

    Adds overlay effect

--- js/script.js
+++ js/script.js
@@ -6,13 +6,16 @@ const openIconImg = './assets/images/icon-menu.svg';
 
 const closeIconImg = './assets/images/icon-menu-close.svg';
 
+const overlay = document.querySelector('.overlay');
+
 const navSlider = () => {
   menuIcon.addEventListener('click', () => {
     menu.classList.toggle('nav-show');
+    overlay.classList.toggle('overlay-show');
 
-    const icon = menuIcon.getAttribute('src');
+    let menuIconSrc = menuIcon.getAttribute('src');
 
-    if (icon === openIconImg) {
+    if (menuIconSrc === openIconImg) {
       menuIcon.setAttribute('src', closeIconImg);
     } else {
       menuIcon.setAttribute('src', openIconImg);
